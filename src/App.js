fb0dadd44978eb0b99bb9316d8f562e9d2e915a0
MH4000 <MH4000@github.com>
2020-01-20 00:52:12 -0800

    finishing logic and added favicon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import './App.css';
+import Form from './components/Form/form.js';
+import Images from './components/Images/images.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends React.Component{
+	state = {
+		image_link: undefined,
+		image_url: undefined,
+		image_size: undefined
+	}
+	resetImages=(e)=>{
+		e.preventDefault();
+		this.setState({
+			image_link: undefined,
+			image_url: undefined,
+			image_size: undefined
+		})
+	}
+	setImgSize=(i,e)=>{
+		e.preventDefault();
+		this.setState({
+			image_size: i
+		})
+	}
+	getImages = async (e) =>{
+		e.preventDefault();
+		const apiUrlSize = [];
+		const apiUrlSizeRes =[];
+		const apiUrl = await fetch('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=04d2badaf65b76589e9d498ba4eba00a&per_page=10&format=json&nojsoncallback=1')
+		const apiUrlRes = await apiUrl.json()
+		for (var id in apiUrlRes.photos.photo){
+			apiUrlSize.push(await fetch('https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=04d2badaf65b76589e9d498ba4eba00a&photo_id='+apiUrlRes.photos.photo[id].id+'&format=json&nojsoncallback=1'))
+			apiUrlSizeRes.push(await apiUrlSize[id].json());
+		}
+		this.setState({
+			image_link: apiUrlSizeRes.map(url => url.sizes.size.map(link => link.url)),
+			image_url: apiUrlSizeRes.map(url => url.sizes.size.map(link => link.source)),
+			image_width: apiUrlSizeRes.map(url => url.sizes.size.map(link => link.width))
+		});
+	}
+	render() {
+		document.title="Flickr App";
+		return (
+			<div className="App">
+				<Form className="input_form"
+					img={this.getImages}
+					setSize={this.setImgSize}
+					reset={this.resetImages}/>
+				<Images img={this.state['image_url']}
+					link={this.state['image_link']}
+					width={this.state['image_width']}
+					id={this.state['id']}
+					size={this.state['image_size']}/>
+			</div>
+		);
+	}
 }
-
 export default App;
--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,49 +1,45 @@
 import React from 'react';
+import './form.css'
 const Form = (props) => {
-	//console.log(props);
-  return (
+	return (
 		<form>
-				<div className="container">
-					<div className="row">
-						<div className="col-md-12">
-							<h2>Select size of photos (in pixels) you want them to be displayed</h2>
-						</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						<h2>Select the size of photos as you want them to be displayed</h2>
 					</div>
-					<div className="row">
-						<div className="col-md-2">
-							<button type="button" className="btn btn-primary" id="sq" onClick={(e) => {props.setSize(75)}}>Square</button>
-						</div>
-						<div className="col-md-2">
-							<button type="button" className="btn btn-primary" id="lg-sq" onClick={(e) => {props.setSize(150)}}>Large Square</button>
-						</div>
-						<div className="col-md-2">
-							<button type="button" className="btn btn-primary" id="thumb" onClick={(e) => {props.setSize(100)}}>Thumbnail</button>
-						</div>
-						<div className="col-md-2">
-							<button type="button" className="btn btn-primary" id="small" onClick={(e) => {props.setSize(240)}}>Small</button>
-						</div>
-						<div className="col-md-2">
-							<button type="button" className="btn btn-primary" id="mid" onClick={(e) => {props.setSize(500)}}>Medium</button>
-						</div>
-						<div className="col-md-2">
-							<button type="button" className="btn btn-primary" id="ori" onClick={(e) => {props.setSize(640)}}>Original</button>
-						</div>
+				</div>
+				<div className="row">
+					<div className="col-md-2">
+						<button type="button" className="btn btn-primary" id="sq" onClick={(e) => {props.setSize(75,e)}}>Square </button>
 					</div>
-					<div className="row">
-						<div className="col-md-12">
-							<h2>Hit This button to fetch photos</h2>
-							<button type="button" className="btn btn-success" id="button" onClick={(e) => {props.img(e)}}>Fetch Recent Photos</button>
-							<button type="button" className="btn btn-alert" id="reset">Reset</button>
-							<hr/>
-						</div>
+					<div className="col-md-2">
+						<button type="button" className="btn btn-primary" id="lg-sq" onClick={(e) => {props.setSize(150,e)}}>Large Square</button>
 					</div>
-					<div className="row">
-						<div className="col-md-12">
-							<div id="results"></div>
-							</div>
-						</div>
+					<div className="col-md-2">
+						<button type="button" className="btn btn-primary" id="thumb" onClick={(e) => {props.setSize(100,e)}}>Thumbnail</button>
 					</div>
-				</form>
-  )
+					<div className="col-md-2">
+						<button type="button" className="btn btn-primary" id="small" onClick={(e) => {props.setSize(240,e)}}>Small</button>
+					</div>
+					<div className="col-md-2">
+						<button type="button" className="btn btn-primary" id="mid" onClick={(e) => {props.setSize(500,e)}}>Medium</button>
+					</div>
+					<div className="col-md-2">
+						<button type="button" className="btn btn-primary" id="ori" onClick={(e) => {props.setSize(640,e)}}>Original</button>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-12">
+						<h2>Hit This button to fetch photos</h2>
+						<p>Up to ten photos will be displayed. Clicking on a photo will take you to the Flickr page containing the photo.</p>
+						<button type="button" className="btn btn-success" id="button" onClick={(e) => {props.img(e)}}>Fetch Recent Photos</button>
+						<button type="button" className="btn btn-alert" id="reset"onClick={(e) => {props.reset(e)}}>Reset</button>
+						<hr/>
+					</div>
+				</div>
+			</div>
+		</form>
+	)
 }
 export default Form;
